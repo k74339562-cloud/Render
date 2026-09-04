88eba7be5c1e66de6eef9d89558e1616b2d26b14
k74339562-cloud <k74339562@gmail.com>
2026-09-04 22:55:56 +0300

    Update renderer.h

--- src/renderer.h
+++ src/renderer.h
@@ -1,27 +1,80 @@
 #pragma once
-#include <EGL/egl.h>
-#include <GLES3/gl3.h>
+#include <vulkan/vulkan.h>
 #include <android/native_window.h>
+#include <vector>
 #include "camera.h"
 #include "mesh.h"
 #include "gizmo.h"
 
-class Renderer {
+struct UniformBufferObject {
+    Mat4 viewProj;
+    Mat4 model;
+    Vec3 camPos;
+    float padding = 0.0f;
+};
+
+class VulkanRenderer {
 public:
-    EGLDisplay display = EGL_NO_DISPLAY;
-    EGLSurface surface = EGL_NO_SURFACE;
-    EGLContext context = EGL_NO_CONTEXT;
-    int width = 0, height = 0;
+    VkInstance instance = VK_NULL_HANDLE;
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device = VK_NULL_HANDLE;
+    VkQueue graphicsQueue = VK_NULL_HANDLE;
+    uint32_t queueFamilyIndex = 0;
+
+    VkSwapchainKHR swapchain = VK_NULL_HANDLE;
+    VkFormat swapchainFormat = VK_FORMAT_B8G8R8A8_UNORM;
+    VkExtent2D swapchainExtent = {0, 0};
+    std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
+
+    VkImage depthImage = VK_NULL_HANDLE;
+    VkDeviceMemory depthImageMemory = VK_NULL_HANDLE;
+    VkImageView depthImageView = VK_NULL_HANDLE;
+
+    VkRenderPass renderPass = VK_NULL_HANDLE;
+    std::vector<VkFramebuffer> framebuffers;
+
+    VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
+    VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
+    VkDescriptorSet descriptorSet = VK_NULL_HANDLE;
+
+    VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+    VkPipeline cubePipeline = VK_NULL_HANDLE;
+    VkPipeline linePipeline = VK_NULL_HANDLE;
+    VkPipeline gizmoPipeline = VK_NULL_HANDLE;
+
+    VkCommandPool commandPool = VK_NULL_HANDLE;
+    std::vector<VkCommandBuffer> commandBuffers;
+
+    VkSemaphore imageAvailableSemaphore = VK_NULL_HANDLE;
+    VkSemaphore renderFinishedSemaphore = VK_NULL_HANDLE;
+    VkFence inFlightFence = VK_NULL_HANDLE;
+
+    VkBuffer uboBuffer = VK_NULL_HANDLE;
+    VkDeviceMemory uboBufferMemory = VK_NULL_HANDLE;
+
+    VkBuffer cubeVbo = VK_NULL_HANDLE;
+    VkDeviceMemory cubeVboMemory = VK_NULL_HANDLE;
+    VkBuffer cubeIbo = VK_NULL_HANDLE;
+    VkDeviceMemory cubeIboMemory = VK_NULL_HANDLE;
+
+    VkBuffer gridVbo = VK_NULL_HANDLE;
+    VkDeviceMemory gridVboMemory = VK_NULL_HANDLE;
+    uint32_t gridVertexCount = 0;
+
+    VkBuffer gizmoVbo = VK_NULL_HANDLE;
+    VkDeviceMemory gizmoVboMemory = VK_NULL_HANDLE;
+    uint32_t gizmoVertexCount = 0;
 
     Camera camera;
-    Mesh mesh;
     Gizmo gizmo;
 
-    GLuint cubeShader = 0;
-    GLuint lineShader = 0;
-
-    bool initEGL(ANativeWindow* window);
-    void destroyEGL();
-    void initShaders();
+    bool init(ANativeWindow* window);
+    void cleanup();
     void renderFrame();
+
+private:
+    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
 };
