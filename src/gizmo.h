4576ca01249e3c0ca9f91dd5be289dfb7d01dc7b
k74339562-cloud <k74339562@gmail.com>
2026-09-04 22:52:50 +0300

    Update gizmo.h

--- src/gizmo.h
+++ src/gizmo.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "math_3d.h"
-#include <GLES3/gl3.h>
+#include <vector>
+
+struct GizmoVertex {
+    float x, y, z;
+    float r, g, b, a;
+};
 
 class Gizmo {
 public:
-    GLuint vao = 0;
-    GLuint vbo = 0;
-    int activeAxis = -1;
+    std::vector<GizmoVertex> vertices;
 
     void init();
-    void render(const Mat4& vp);
-    void checkHit(const Vec3& rayOrigin, const Vec3& rayDir, const Vec3& cubePos);
+    Mat4 getTransform(const Vec3& cubePos, float camDist) const;
 };
