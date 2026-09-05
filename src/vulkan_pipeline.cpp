242f2915c2ae79cb7c780b80253dd87fda3e75f2
k74339562-cloud <k74339562@gmail.com>
2026-09-05 08:45:17 +0300

    Create vulkan_pipeline.cpp

---
+++ src/vulkan_pipeline.cpp
@@ -0,0 +1,272 @@
+#include "vulkan_pipeline.h"
+#include <cstddef>
+#include "shaders/cube_vert.h"
+#include "shaders/cube_frag.h"
+#include "shaders/line_vert.h"
+#include "shaders/line_frag.h"
+
+struct VertexCube { float x, y, z; float nx, ny, nz; };
+struct VertexLine { float x, y, z; float r, g, b, a; };
+
+static VkShaderModule createShaderModule(VkDevice device, const uint32_t* code, size_t sizeBytes) {
+    VkShaderModuleCreateInfo ci{VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO};
+    ci.codeSize = sizeBytes;
+    ci.pCode = code;
+    VkShaderModule mod = VK_NULL_HANDLE;
+    vkCreateShaderModule(device, &ci, nullptr, &mod);
+    return mod;
+}
+
+uint32_t VulkanPipeline::findMemoryType(VkPhysicalDevice physicalDevice, uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+    VkPhysicalDeviceMemoryProperties memProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+bool VulkanPipeline::init(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat swapchainFormat, 
+                          VkExtent2D extent, const std::vector<VkImageView>& swapchainImageViews, VkBuffer uboBuffer) {
+    // 1. إنشاء صورة العمق (Depth Image)
+    VkImageCreateInfo depthImgInfo{VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO};
+    depthImgInfo.imageType = VK_IMAGE_TYPE_2D;
+    depthImgInfo.extent = { extent.width, extent.height, 1 };
+    depthImgInfo.mipLevels = 1;
+    depthImgInfo.arrayLayers = 1;
+    depthImgInfo.format = VK_FORMAT_D24_UNORM_S8_UINT;
+    depthImgInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+    depthImgInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+    vkCreateImage(device, &depthImgInfo, nullptr, &depthImage);
+
+    VkMemoryRequirements dMemReq;
+    vkGetImageMemoryRequirements(device, depthImage, &dMemReq);
+    VkMemoryAllocateInfo dAlloc{VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO};
+    dAlloc.allocationSize = dMemReq.size;
+    dAlloc.memoryTypeIndex = findMemoryType(physicalDevice, dMemReq.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    vkAllocateMemory(device, &dAlloc, nullptr, &depthImageMemory);
+    vkBindImageMemory(device, depthImage, depthImageMemory, 0);
+
+    VkImageViewCreateInfo dViewInfo{VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO};
+    dViewInfo.image = depthImage;
+    dViewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    dViewInfo.format = VK_FORMAT_D24_UNORM_S8_UINT;
+    dViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+    dViewInfo.subresourceRange.levelCount = 1;
+    dViewInfo.subresourceRange.layerCount = 1;
+    vkCreateImageView(device, &dViewInfo, nullptr, &depthImageView);
+
+    // 2. إنشاء RenderPass
+    VkAttachmentDescription attachments[2] = {};
+    attachments[0].format = swapchainFormat;
+    attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
+    attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    attachments[0].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    attachments[1].format = VK_FORMAT_D24_UNORM_S8_UINT;
+    attachments[1].samples = VK_SAMPLE_COUNT_1_BIT;
+    attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+    VkAttachmentReference colorRef{0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL};
+    VkAttachmentReference depthRef{1, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
+    VkSubpassDescription subpass{};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorRef;
+    subpass.pDepthStencilAttachment = &depthRef;
+
+    VkRenderPassCreateInfo rpci{VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO};
+    rpci.attachmentCount = 2;
+    rpci.pAttachments = attachments;
+    rpci.subpassCount = 1;
+    rpci.pSubpasses = &subpass;
+    vkCreateRenderPass(device, &rpci, nullptr, &renderPass);
+
+    // 3. إنشاء الـ Framebuffers
+    framebuffers.resize(swapchainImageViews.size());
+    for (size_t i = 0; i < swapchainImageViews.size(); i++) {
+        VkImageView fbViews[] = { swapchainImageViews[i], depthImageView };
+        VkFramebufferCreateInfo fbci{VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO};
+        fbci.renderPass = renderPass;
+        fbci.attachmentCount = 2;
+        fbci.pAttachments = fbViews;
+        fbci.width = extent.width;
+        fbci.height = extent.height;
+        fbci.layers = 1;
+        vkCreateFramebuffer(device, &fbci, nullptr, &framebuffers[i]);
+    }
+
+    // 4. Descriptor Set للـ UBO
+    VkDescriptorSetLayoutBinding uboBinding{};
+    uboBinding.binding = 0;
+    uboBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uboBinding.descriptorCount = 1;
+    uboBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+
+    VkDescriptorSetLayoutCreateInfo dslci{VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO};
+    dslci.bindingCount = 1;
+    dslci.pBindings = &uboBinding;
+    vkCreateDescriptorSetLayout(device, &dslci, nullptr, &descriptorSetLayout);
+
+    VkDescriptorPoolSize poolSize{VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1};
+    VkDescriptorPoolCreateInfo dpci{VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO};
+    dpci.poolSizeCount = 1;
+    dpci.pPoolSizes = &poolSize;
+    dpci.maxSets = 1;
+    vkCreateDescriptorPool(device, &dpci, nullptr, &descriptorPool);
+
+    VkDescriptorSetAllocateInfo dsai{VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO};
+    dsai.descriptorPool = descriptorPool;
+    dsai.descriptorSetCount = 1;
+    dsai.pSetLayouts = &descriptorSetLayout;
+    vkAllocateDescriptorSets(device, &dsai, &descriptorSet);
+
+    VkDescriptorBufferInfo dbi{uboBuffer, 0, 144};
+    VkWriteDescriptorSet wds{VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET};
+    wds.dstSet = descriptorSet;
+    wds.dstBinding = 0;
+    wds.descriptorCount = 1;
+    wds.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    wds.pBufferInfo = &dbi;
+    vkUpdateDescriptorSets(device, 1, &wds, 0, nullptr);
+
+    // 5. خطوط أنابيب الرسوميات (Pipelines)
+    VkPipelineLayoutCreateInfo plci{VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO};
+    plci.setLayoutCount = 1;
+    plci.pSetLayouts = &descriptorSetLayout;
+    vkCreatePipelineLayout(device, &plci, nullptr, &pipelineLayout);
+
+    VkViewport viewport{ 0.0f, (float)extent.height, (float)extent.width, -(float)extent.height, 0.0f, 1.0f };
+    VkRect2D scissor{{0, 0}, extent};
+    VkPipelineViewportStateCreateInfo pvsi{VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO};
+    pvsi.viewportCount = 1;
+    pvsi.pViewports = &viewport;
+    pvsi.scissorCount = 1;
+    pvsi.pScissors = &scissor;
+
+    // (أ) بايبلاين شيدر استوديو بلندر للمكعب
+    VkShaderModule cVs = createShaderModule(device, cube_vert_data, sizeof(cube_vert_data));
+    VkShaderModule cFs = createShaderModule(device, cube_frag_data, sizeof(cube_frag_data));
+
+    VkPipelineShaderStageCreateInfo cStages[2] = {};
+    cStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    cStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT; cStages[0].module = cVs; cStages[0].pName = "main";
+    cStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    cStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT; cStages[1].module = cFs; cStages[1].pName = "main";
+
+    VkVertexInputBindingDescription cVibd{0, sizeof(VertexCube), VK_VERTEX_INPUT_RATE_VERTEX};
+    VkVertexInputAttributeDescription cViad[2] = {
+        {0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(VertexCube, x)},
+        {1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(VertexCube, nx)}
+    };
+
+    VkPipelineVertexInputStateCreateInfo cPvisi{VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO};
+    cPvisi.vertexBindingDescriptionCount = 1; cPvisi.pVertexBindingDescriptions = &cVibd;
+    cPvisi.vertexAttributeDescriptionCount = 2; cPvisi.pVertexAttributeDescriptions = cViad;
+
+    VkPipelineInputAssemblyStateCreateInfo cPiasi{VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO};
+    cPiasi.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+
+    VkPipelineRasterizationStateCreateInfo prsi{VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO};
+    prsi.cullMode = VK_CULL_MODE_BACK_BIT;
+    prsi.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    prsi.lineWidth = 1.0f;
+
+    VkPipelineMultisampleStateCreateInfo pmssi{VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO};
+    pmssi.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    VkPipelineDepthStencilStateCreateInfo pdssi{VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO};
+    pdssi.depthTestEnable = VK_TRUE; pdssi.depthWriteEnable = VK_TRUE; pdssi.depthCompareOp = VK_COMPARE_OP_LESS;
+
+    VkPipelineColorBlendAttachmentState cbas{};
+    cbas.colorWriteMask = 0xF;
+    VkPipelineColorBlendStateCreateInfo pcbsi{VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO};
+    pcbsi.attachmentCount = 1; pcbsi.pAttachments = &cbas;
+
+    VkGraphicsPipelineCreateInfo cGpci{VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO};
+    cGpci.stageCount = 2; cGpci.pStages = cStages;
+    cGpci.pVertexInputState = &cPvisi; cGpci.pInputAssemblyState = &cPiasi;
+    cGpci.pViewportState = &pvsi; cGpci.pRasterizationState = &prsi;
+    cGpci.pMultisampleState = &pmssi; cGpci.pDepthStencilState = &pdssi;
+    cGpci.pColorBlendState = &pcbsi; cGpci.layout = pipelineLayout; cGpci.renderPass = renderPass;
+    vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &cGpci, nullptr, &cubePipeline);
+
+    vkDestroyShaderModule(device, cVs, nullptr);
+    vkDestroyShaderModule(device, cFs, nullptr);
+
+    // (ب) بايبلاين خطوط شبكة الأرضية وحواف المكعب الداكنة
+    VkShaderModule lVs = createShaderModule(device, line_vert_data, sizeof(line_vert_data));
+    VkShaderModule lFs = createShaderModule(device, line_frag_data, sizeof(line_frag_data));
+
+    VkPipelineShaderStageCreateInfo lStages[2] = {};
+    lStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    lStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT; lStages[0].module = lVs; lStages[0].pName = "main";
+    lStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    lStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT; lStages[1].module = lFs; lStages[1].pName = "main";
+
+    VkVertexInputBindingDescription lVibd{0, sizeof(VertexLine), VK_VERTEX_INPUT_RATE_VERTEX};
+    VkVertexInputAttributeDescription lViad[2] = {
+        {0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(VertexLine, x)},
+        {1, 0, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(VertexLine, r)}
+    };
+
+    VkPipelineVertexInputStateCreateInfo lPvisi{VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO};
+    lPvisi.vertexBindingDescriptionCount = 1; lPvisi.pVertexBindingDescriptions = &lVibd;
+    lPvisi.vertexAttributeDescriptionCount = 2; lPvisi.pVertexAttributeDescriptions = lViad;
+
+    VkPipelineInputAssemblyStateCreateInfo lPiasi{VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO};
+    lPiasi.topology = VK_PRIMITIVE_TOPOLOGY_LINE_LIST;
+
+    VkPipelineRasterizationStateCreateInfo lPrsi = prsi;
+    lPrsi.cullMode = VK_CULL_MODE_NONE;
+    lPrsi.lineWidth = 2.0f;
+
+    VkGraphicsPipelineCreateInfo lGpci = cGpci;
+    lGpci.pStages = lStages; lGpci.pVertexInputState = &lPvisi;
+    lGpci.pInputAssemblyState = &lPiasi; lGpci.pRasterizationState = &lPrsi;
+    vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &lGpci, nullptr, &linePipeline);
+
+    // (ج) بايبلاين الجزمو الصلب (مثلثات تظهر دائماً في الواجهة)
+    VkPipelineInputAssemblyStateCreateInfo gPiasi{VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO};
+    gPiasi.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+
+    VkPipelineDepthStencilStateCreateInfo gPdssi = pdssi;
+    gPdssi.depthTestEnable = VK_FALSE; // فوق كل شيء كبلندر
+
+    VkGraphicsPipelineCreateInfo gGpci = lGpci;
+    gGpci.pInputAssemblyState = &gPiasi; gGpci.pDepthStencilState = &gPdssi;
+    vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &gGpci, nullptr, &gizmoPipeline);
+
+    vkDestroyShaderModule(device, lVs, nullptr);
+    vkDestroyShaderModule(device, lFs, nullptr);
+
+    return true;
+}
+
+void VulkanPipeline::cleanup(VkDevice device) {
+    if (device == VK_NULL_HANDLE) return;
+
+    vkDestroyPipeline(device, gizmoPipeline, nullptr);
+    vkDestroyPipeline(device, linePipeline, nullptr);
+    vkDestroyPipeline(device, cubePipeline, nullptr);
+    vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+
+    vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+    vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+
+    for (auto fb : framebuffers) vkDestroyFramebuffer(device, fb, nullptr);
+    framebuffers.clear();
+
+    vkDestroyRenderPass(device, renderPass, nullptr);
+
+    vkDestroyImageView(device, depthImageView, nullptr);
+    vkDestroyImage(device, depthImage, nullptr);
+    vkFreeMemory(device, depthImageMemory, nullptr);
+}
