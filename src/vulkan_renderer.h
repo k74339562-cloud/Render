324c74d9cc4ab3830a4fbc1a976b841b82291519
k74339562-cloud <k74339562@gmail.com>
2026-09-05 20:01:12 +0300

    Update vulkan_renderer.h

--- src/vulkan_renderer.h
+++ src/vulkan_renderer.h
@@ -1,64 +1,33 @@
 #pragma once
-#include <vulkan/vulkan.h>
 #include <android/native_window.h>
 #include <vector>
 #include "camera.h"
 #include "gizmo.h"
 #include "box.h"
-#include "vulkan_pipeline.h"
+#include "render_engine.h"
 
 enum GizmoAxis {
     AXIS_NONE = 0,
     AXIS_X,
     AXIS_Y,
     AXIS_Z,
-    AXIS_CENTER // إضافة محور المركز الحر (View-Plane Translation)
-};
-
-struct UniformBufferObject {
-    Mat4 viewProj;
-    Vec3 camPos;
-    float padding = 0.0f;
+    AXIS_CENTER
 };
 
 class VulkanRenderer {
 public:
-    VkInstance instance = VK_NULL_HANDLE;
-    VkSurfaceKHR surface = VK_NULL_HANDLE;
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-    VkDevice device = VK_NULL_HANDLE;
-    VkQueue graphicsQueue = VK_NULL_HANDLE;
+    RenderEngine engine; // المحرك المستقل المنفصل بالكامل
+    Camera camera;
+    Gizmo gizmo;
+    Box box;
 
-    VkSwapchainKHR swapchain = VK_NULL_HANDLE;
-    VkFormat swapchainFormat = VK_FORMAT_R8G8B8A8_UNORM;
-    VkExtent2D swapchainExtent = {0, 0};
-    std::vector<VkImage> swapchainImages;
-    std::vector<VkImageView> swapchainImageViews;
-
-    VulkanPipeline pipeline;
-
-    VkCommandPool commandPool = VK_NULL_HANDLE;
-    std::vector<VkCommandBuffer> commandBuffers;
-    VkSemaphore imageAvailableSemaphore = VK_NULL_HANDLE;
-    VkSemaphore renderFinishedSemaphore = VK_NULL_HANDLE;
-    VkFence inFlightFence = VK_NULL_HANDLE;
-
-    VkBuffer uboBuffer = VK_NULL_HANDLE;
-    VkDeviceMemory uboBufferMemory = VK_NULL_HANDLE;
-
-    // شبكة الأرضية
     VkBuffer gridVbo = VK_NULL_HANDLE;
     VkDeviceMemory gridVboMemory = VK_NULL_HANDLE;
     uint32_t gridVertexCount = 0;
 
-    // الجزمو
     VkBuffer gizmoVbo = VK_NULL_HANDLE;
     VkDeviceMemory gizmoVboMemory = VK_NULL_HANDLE;
     uint32_t gizmoVertexCount = 0;
-
-    Camera camera;
-    Gizmo gizmo;
-    Box box;
 
     GizmoAxis activeAxis = AXIS_NONE;
 
@@ -66,11 +35,6 @@ public:
     void cleanup();
     void renderFrame();
 
-    // فحص وسحب الجزمو (بما فيه المركز الحر)
     GizmoAxis testGizmoHit(float touchX, float touchY, float screenW, float screenH);
     void dragGizmo(float dx, float dy, float screenW, float screenH);
-
-private:
-    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
-    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
 };
