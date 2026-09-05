096db68217c2c29ff7a611018c4e25f65f71fc06
k74339562-cloud <k74339562@gmail.com>
2026-09-05 09:57:58 +0300

    Update camera.h

--- src/camera.h
+++ src/camera.h
@@ -3,10 +3,10 @@
 
 class Camera {
 public:
-    Vec3 ofs = {0.0f, 0.0f, 0.0f}; // نقطة ارتكاز الدوران (Target)
-    float dist = 7.5f;              // المسافة الابتدائية
-    float yaw = 0.785f;             // زاوية الدوران الأفقية (45 درجة)
-    float pitch = 0.523f;           // زاوية الدوران الرأسية (30 درجة)
+    Vec3 ofs = {0.0f, 0.0f, 0.0f};
+    float dist = 7.2f;
+    float yaw = -0.785f;  // زاوية بلندر الكلاسيكية (-45 درجة)
+    float pitch = 0.523f; // زاوية بلندر الكلاسيكية (30 درجة)
 
     void onOrbit(float dx, float dy);
     void onZoom(float delta);
